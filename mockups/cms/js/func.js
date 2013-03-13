0aff182a7a1f12c6784869b993413f94611b6675
Anthony <anthonyecossins@gmail.com>
2013-03-13 00:32:30 +0000

    New toolbar now at the top with search bar. Basics of search added, can detect comma seperated names
    This commit is dedicated in memory of left sidebar. RIP

--- mockups/cms/js/func.js
+++ mockups/cms/js/func.js
@@ -1,6 +1,102 @@
-function init () {
+var index = [];
+var names = [];
+var suggestions = [];
+var selectedSuggestion = 0;
 
+var searchSelected = false;
+
+function init () {
+	resizeSearch();
+	hideSearch();
 }
 
+function resizeSearch () {
+	var searchLeft = $("#search").offset().left;
+	var rightWidth = $(".right").width();
+	
+	$("#search input").width($(window).width() - searchLeft - rightWidth);
+	$(".search").width($(window).width() - searchLeft - rightWidth);
+}
+
+function showSearch () {
+	searchSelected = true;
+	$(".search").css("display", "block");
+}
+
+function hideSearch () {
+	searchSelected = false;
+	$(".search").css("display", "none");
+}
+
+function isAllowSearch () {
+	if($("#search input").val() != "") {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function trimEnds (str) {
+    str = str.replace(/^\s+/, '');
+    for (var i = str.length - 1; i >= 0; i--) {
+        if (/\S/.test(str.charAt(i))) {
+            str = str.substring(0, i + 1);
+            break;
+        }
+    }
+    return str;
+}
+
+function parseString (string) {
+	nameArray = string.split(",");
+	console.log(nameArray.length);
+	
+	for (i=0; i<nameArray.length; i++) {
+		nameArray[i] = trimEnds(nameArray[i]);
+		var currentName = nameArray[i]
+		
+		if (nameArray[i] == "" || nameArray[i] == " ") {
+			nameArray.splice(i, 1);
+		}
+		
+		console.log(currentName);	
+	}
+	
+	suggestions = nameArray;
+}
+
+$("#search input").focus(function() {
+	if (isAllowSearch()) {
+		showSearch();	
+	}
+});
+
+$("#search input").blur(function() {
+	hideSearch();	
+});
+
+
+$("#search input").keyup(function() {
+	if($("#search input").val() != "") {
+		showSearch();
+		parseString($("#search input").val());
+		
+		
+		$(".search .add span").html("Add ");
+			
+		for (i=0; i<suggestions.length; i++) {
+			$(".search .add span").append("<a class='name'>"+suggestions[i]+"</a>");
+		}
+		
+		$(".search .add span").append(" as a user");
+	
+	}else{
+		hideSearch();	
+	}
+});
+
+$(window).resize(function() {
+	resizeSearch();
+});
 
 init();
